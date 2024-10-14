1fde6bc9d7c9651e5ad9f8492471927669149d13
Arthur Sampaio <113317860+sampaio-arthh@users.noreply.github.com>
2024-10-14 15:42:45 -0300

    Update versão final

--- stock/test.sql
+++ stock/test.sql
@@ -1,3 +1,4 @@
+DROP DATABASE pokemon;
 CREATE DATABASE pokemon;
 USE pokemon;
 CREATE TABLE pokemon (
@@ -5,7 +6,7 @@ CREATE TABLE pokemon (
     nome VARCHAR(100) NOT NULL,
     tipo VARCHAR(50) NOT NULL,
    	nvl INT NOT NULL,
-    link varchar(200)
+    link varchar(300) NOT NULL
 );
 CREATE TABLE treinador (
     id_treinador INT PRIMARY KEY,
@@ -34,30 +35,29 @@ CREATE TABLE batalha (
     data DATE NOT NULL
 );
 
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (1, "Pikachu", "elétrico", 10, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png");
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (2, "Charmander", "fogo", 8, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png");
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (1, "Pikachu", "elétrico", 10, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png");
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (2, "Charmander", "fogo", 8, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png");
 INSERT INTO Treinador (id_treinador, nome, idade, cidade) VALUES (1, "Ash", 10, "Kanto");
 
-/*
 INSERT INTO Treinador_pokemon (id_treinador, id_pokemon) VALUES (1,1);
 INSERT INTO Treinador_pokemon (id_treinador, id_pokemon) VALUES (1,2);
 
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (3, "Bulbasaur", "grama/veneno", 10);
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (3, "Bulbasaur", "grama/veneno", 10, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png");
 INSERT INTO Treinador (id_treinador, nome, idade, cidade) VALUES (2, "Misty", 12, "Cerulean");
 
 INSERT INTO Treinador_pokemon (id_treinador, id_pokemon) VALUES (2,3);
 
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (4, "Squirtle", "água", 9);
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (4, "Squirtle", "água", 9, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png");
 INSERT INTO Treinador (id_treinador, nome, idade, cidade) VALUES (3, "Brock", 15, "Pewter");
 
 INSERT INTO Treinador_pokemon (id_treinador, id_pokemon) VALUES (3,4);
 
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (5, "Arceus", "normal", 100);
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (6, "Rayquaza", "dragão/voador", 100);
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (7, "Darkrai", "sombrio", 100);
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (8, "Charizard", "fogo/voador", 100);
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (9, "Golurk", "fantasma/terra", 100);
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (10, "Mewtwo", "psíquico", 100);
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (5, "Arceus", "normal", 100, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/493.png");
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (6, "Rayquaza", "dragão/voador", 100, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/384.png");
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (7, "Darkrai", "sombrio", 100, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/491.png");
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (8, "Charizard", "fogo/voador", 100, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/006.png");
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (9, "Golurk", "fantasma/terra", 100, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/623.png");
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (10, "Mewtwo", "psíquico", 100, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/150.png");
 INSERT INTO Treinador (id_treinador, nome, idade, cidade) VALUES (4, "Arthur", 17, "Pallet");
 
 INSERT INTO Treinador_pokemon (id_treinador, id_pokemon) VALUES (4,5);
@@ -67,7 +67,7 @@ INSERT INTO Treinador_pokemon (id_treinador, id_pokemon) VALUES (4,8);
 INSERT INTO Treinador_pokemon (id_treinador, id_pokemon) VALUES (4,9);
 INSERT INTO Treinador_pokemon (id_treinador, id_pokemon) VALUES (4,10);
 
-INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl) VALUES (11, "Jigglypuff", "fada", 7);
+INSERT INTO Pokemon (id_pokemon, nome, tipo, nvl, link) VALUES (11, "Jigglypuff", "fada", 7, "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/039.png");
 INSERT INTO Treinador (id_treinador, nome, idade, cidade) VALUES (5, "Gary", 11, "Pallet");
 INSERT INTO Treinador_pokemon (id_treinador, id_pokemon) VALUES (5,11);
 
@@ -76,4 +76,3 @@ INSERT INTO Batalha (id_batalha, id_treinador1, id_pokemon1, id_treinador2, id_p
 INSERT INTO Batalha (id_batalha, id_treinador1, id_pokemon1, id_treinador2, id_pokemon2, id_treinador_vencedor, data) VALUES (3,5,11,2,3,2,"2024-09-03");
 
 INSERT INTO Batalha (id_batalha, id_treinador1, id_pokemon1, id_treinador2, id_pokemon2, id_treinador_vencedor, data) VALUES (4,1,1,4,6,4,"2024-09-11");
-*/
