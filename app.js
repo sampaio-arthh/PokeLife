367baf20f3810f4e5120965716652cff73937b72
Arthur Sampaio <113317860+sampaio-arthh@users.noreply.github.com>
2024-10-23 08:52:13 -0300

    Versão Final

--- app.js
+++ app.js
@@ -24,22 +24,9 @@ db.connect((error)=>{
     }
 })
 
-/* app.get("/pokemons", (req, res)=>{
-    //query through db w/ SQL
-    db.query("SELECT nome, nvl FROM Pokemon", (error, results) => {
-    if(error){
-        console.log("Ocorreu um erro ao buscar todos os pokemons", error);
-    }
-    else{
-
-        res.render("/views/pokemons.ejs", {pokemons : results});
-
-        //sem ejs:
-        //res.send(results); //out to page
-        //console.log(results); //out to console
-    }
-})
-}); */
+//sem ejs:
+//res.send(results); //out to page
+//console.log(results); //out to console
 
 app.get("/", (req, res)=>{
     res.render(__dirname + "/views/home.ejs")
@@ -79,7 +66,7 @@ app.get("/trainers", (req, res)=>{
 
 app.get("/pesquisarTrainer", (req, res) =>{
     const pesquisa = req.query.pesquisa;
-    db.query("SELECT t.id_treinador, t.nome, t.idade, t.cidade, COUNT(b.id_treinador_vencedor) AS vitorias FROM treinador t LEFT JOIN batalha b ON t.id_treinador = b.id_treinador_vencedor WHERE t.nome LIKE ? OR t.cidade = ? OR CAST(idade AS CHAR) = ? GROUP BY t.id_treinador, t.nome, t.idade, t.cidade", [`%${pesquisa}%`, pesquisa, pesquisa], (error, results) => {
+    db.query("SELECT t.id_treinador, t.nome, t.idade, t.cidade, COUNT(b.id_treinador_vencedor) AS vitorias FROM treinador t LEFT JOIN batalha b ON t.id_treinador = b.id_treinador_vencedor WHERE t.nome = ? OR t.cidade = ? OR CAST(idade AS CHAR) = ? GROUP BY t.id_treinador, t.nome, t.idade, t.cidade", [`%${pesquisa}%`, pesquisa, pesquisa], (error, results) => {
         if(error){
             console.log("Houve um erro ao realizar a pesquisa\n", error);
         }
@@ -90,8 +77,30 @@ app.get("/pesquisarTrainer", (req, res) =>{
 });
 
 app.get("/battles", (req, res)=>{
-    res.render(__dirname + "/views/battles.ejs")
-})
+    db.query("SELECT b.id_batalha, t1.nome AS treinador1, p1.nome AS pokemon1, t2.nome AS treinador2, p2.nome AS pokemon2, tv.nome AS treinador_vencedor, p1.link AS link1, p2.link AS link2 FROM batalha b JOIN treinador t1 ON b.id_treinador1 = t1.id_treinador JOIN pokemon p1 ON b.id_pokemon1 = p1.id_pokemon JOIN treinador t2 ON b.id_treinador2 = t2.id_treinador JOIN pokemon p2 ON b.id_pokemon2 = p2.id_pokemon JOIN treinador tv ON b.id_treinador_vencedor = tv.id_treinador;", (error, results) =>{
+        if(error){
+            console.log("Houve um erro na busca das batalhas \n", error);
+        }
+        else{
+            res.render("battles", {batalhas : results});
+        }
+    })
+});
+
+app.get("/pesquisarBattles", (req, res) => {
+    const pesquisa = req.query.pesquisa;
+
+    db.query(`
+    SELECT b.id_batalha, t1.nome AS treinador1, p1.nome AS pokemon1, t2.nome AS treinador2, p2.nome AS pokemon2, tv.nome AS treinador_vencedor, p1.link AS link1, p2.link AS link2 FROM batalha b JOIN treinador t1 ON b.id_treinador1 = t1.id_treinador JOIN pokemon p1 ON b.id_pokemon1 = p1.id_pokemon JOIN treinador t2 ON b.id_treinador2 = t2.id_treinador JOIN pokemon p2 ON b.id_pokemon2 = p2.id_pokemon JOIN treinador tv ON b.id_treinador_vencedor = tv.id_treinador WHERE tv.nome LIKE ? OR p1.nome = ? OR p2.nome = ?`, [`%${pesquisa}%`, pesquisa, pesquisa], (error, results) => {
+        if (error) {
+            console.log("Houve um erro ao realizar a pesquisa\n", error);
+            return res.status(500).send("Erro na consulta");
+        } else {
+            res.render("battles", { batalhas: results });
+        }
+    });
+});
+
 
 app.use(express.static(__dirname + '/styles'));
 
